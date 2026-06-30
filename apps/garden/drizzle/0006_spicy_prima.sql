cb157ab30322b2f2abbc9e87acb8306d90ce6975
BanJoeH <jch.harrison@gmail.com>
2026-06-30 16:19:01 +0100

    try again

--- apps/garden/drizzle/0006_spicy_prima.sql
+++ apps/garden/drizzle/0006_spicy_prima.sql
@@ -11,7 +11,7 @@ CREATE TABLE `__new_household_members` (
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-INSERT INTO `__new_household_members`("id", "household_id", "user_id", "email", "created_at", "updated_at", "deleted_at") SELECT "id", "household_id", "user_id", "email", "created_at", "updated_at", "deleted_at" FROM `household_members`;--> statement-breakpoint
+INSERT INTO `__new_household_members`("id", "household_id", "user_id", "email", "created_at", "updated_at", "deleted_at") SELECT "id", "household_id", "user_id", NULL, "created_at", "updated_at", "deleted_at" FROM `household_members`;--> statement-breakpoint
 DROP TABLE `household_members`;--> statement-breakpoint
 ALTER TABLE `__new_household_members` RENAME TO `household_members`;--> statement-breakpoint
 PRAGMA foreign_keys=ON;--> statement-breakpoint
